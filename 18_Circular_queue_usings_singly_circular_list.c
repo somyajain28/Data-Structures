848c182965f37896d1973005145bd7130c64627e
Somya Jain <somyajainpk@gmail.com>
2026-01-16 20:24:05 +0530

    next program

---
+++ 18_Circular_queue_usings_singly_circular_list.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+	int data;
+	struct Node *next;
+}node;
+
+node* enq(node** f,node** r,int v);
+node* deq(node** f, node** r);
+void disp_list(node* f);
+node* create_node(int v);
+
+void main()
+{
+	node* f = NULL;
+	node* r = NULL;
+	int v,ch;
+	do
+	{
+		printf("\n*--MENU--*\n");
+		printf("\n1.Enqueue\n2.Dequeue\n3.Traverse\n4.Exit\n");
+		printf("Enter your choice:");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				printf("Enter the value:");
+				scanf("%d",&v);
+				enq(&f,&r,v);
+				break;
+			case 2:
+				deq(&f,&r);
+				break;
+			case 3:
+				disp_list(f);
+				break;
+			case 4:
+				exit(0);
+			default:
+				printf("Invalid Choice");				
+		}
+	}while(1);
+}
+
+node* create_node(int v)
+{
+	node* nn;
+	nn=(node*)malloc(sizeof(node));
+	if(nn == NULL)
+	{
+		printf("ERROR! Memory can not be allocated\n");
+		return nn;
+	}
+	nn->data = v;
+	nn->next = NULL;
+	return nn;
+}
+
+node* enq(node** f,node** r,int v)
+{
+	node *nn;
+	nn = create_node(v);
+	if(*r == NULL)
+	{
+		*f = nn;
+		*r = nn;
+		(*r)->next = (*f);
+	}
+	else
+	{
+		nn->next = (*f);
+		(*r)->next = nn;
+		*r = nn;
+	}
+}
+node* deq(node** f, node** r)
+{
+	node *temp =*f;
+	int v;
+	if(*f == NULL)
+	{
+		printf("Queue is empty");
+		return;
+	}
+	v = temp->data;
+	printf("the dequed element is: %d",v);
+	if(*f==*r)
+	{
+		*f=NULL;
+		*r=NULL;
+	}
+	else
+	{
+		*f=(*f)->next;
+		(*r)->next = *f;
+	}
+	free(temp);
+}
+
+void disp_list(node* f)
+{
+	node* p;
+	if(f == NULL)
+	{
+		printf("Queue is empty");
+		return;
+	}
+	p = f;
+	do
+	{
+		printf("%d ",p->data);
+		p = p->next;
+	}while(p != f);
+}
